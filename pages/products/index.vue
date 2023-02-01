960675877d2da4706c09121f08ad86ac10b0bbdb
murimichege <chegeeddie@gmail.com>
2023-02-01 10:42:55 +0300

    initial commit

---
+++ pages/products/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in products">
+                <ProductCard :product="p" />
+                <!-- <NuxtLink :to="`/products/${p.id}`">
+                {{ p.title }}
+            </NuxtLink> -->
+
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'products'
+})
+//fetch products
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+
+useHead({
+    title: 'Nuxt Store | Products',
+    meta: [
+        {
+            name: 'description', content: 'Nuxt 3 Merch'
+        }
+    ]
+})
+
+</script>
+
+<style scoped>
+
+</style>
